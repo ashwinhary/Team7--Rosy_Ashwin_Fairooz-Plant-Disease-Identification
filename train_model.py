06dcd9998f17ed20f217443fa2212c01cf68d40f
Team7 _ Rosy_Ashwin_Fairooz <ashwinhary@gmail.com>
2025-03-17 21:51:31 +0800

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.applications import EfficientNetB0
+from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Dropout
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
@@ -58,7 +58,7 @@ val_generator = val_datagen.flow_from_directory(
 # ========================== #
 
 # Base Model: EfficientNetB0
-base_model = EfficientNetB0(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+base_model = MobileNetV2(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
 
 # Freeze base model layers initially
 base_model.trainable = False
